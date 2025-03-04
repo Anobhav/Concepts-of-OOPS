2013281d76154173df83a0206fba287f3dfbc61a
Anobhav Singh <63008107+Anobhav@users.noreply.github.com>
2025-03-04 17:59:52 +0530

    added

---
+++ friend_function.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+class Complex{
+    int a,b;
+    public:
+        void setNumber(int num1, int num2)
+        {
+            a=num1;
+            b=num2;
+        }
+        void printnumber()
+        {
+            cout<<"number is: "<<a<<"+"<<b<<"i"<<endl;
+        }
+        friend Complex sumComplex(Complex o1, Complex o2);
+};
+
+Complex sumComplex (Complex o1, Complex o2)
+{
+    Complex o3;
+    o3.setNumber(o1.a + o2.a,o1.b + o2.b);
+    return o3;
+}
+
+int main()
+{
+    Complex c1,c2,sum;
+    c1.setNumber(1,4);
+    c2.setNumber(5,8);
+
+    c1.printnumber();
+    c2.printnumber();
+    
+    sum=sumComplex(c1,c2);
+    sum.printnumber();
+    return 0;
+}
