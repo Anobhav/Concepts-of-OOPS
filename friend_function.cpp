b8ce1552b3d43e996c21652278e12d3e765da5e0
Anobhav Singh <63008107+Anobhav@users.noreply.github.com>
2025-03-04 18:06:01 +0530

    Updated Complex class to fix summation error

--- friend_function.cpp
+++ friend_function.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 using namespace std;
-
+/*
+    properties of friend function 
+    1. allows a non member of the class to use private variables of the class
+    2. since friend function is not in the scope of the class it cannot be called from the object of that class
+    3. can be invoked without help of any object 
+    4.  usually contains objects as arguments 
+    5. it cannot access memebers directly by their names and need object_name.member_name to access them 
+*/
 class Complex{
     int a,b;
     public:
